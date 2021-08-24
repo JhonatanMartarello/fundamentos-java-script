fab500f8d71811b6fa1b102e4f3505ab7513a8dc
JhonatanMartarello <jhonatan.martarello@gmail.com>
2021-08-24 20:58:45 -0300

    date methods

---
+++ date.js
@@ -0,0 +1,59 @@
+const date = new Date();
+console.log(date);
+
+//7 numbers specify year, month, day, hour, minute, second, and millisecond (in that order):
+const d = new Date(2018, 11, 24, 12, 33, 30, 0);
+console.log(d);
+
+const dataString = new Date("October 13, 2014 11:13:00");
+console.log(dataString);
+
+
+const dateUtcString = new Date();
+console.log(d.toUTCString());
+
+const dateToString = new Date();
+console.log(dateToString.toDateString());
+
+const dateIso = new Date();
+console.log(dateIso.toISOString());
+
+let msec = Date.parse("March 21, 2012");
+console.log(msec);
+const dataConvertida = new Date(msec);
+console.log(dataConvertida);
+
+// the first month (January) is month number 0, so December returns month number 11.
+console.log(date.getMonth());
+console.log(date.getFullYear());
+
+const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+console.log(months[date.getMonth()]);
+
+// The getDay() method returns the weekday of a date as a number (0-6).
+// The first day of the week (0) means "Sunday", even if some countries in the world consider the first day of the week to be "Monday"
+console.log(date.getDay());
+const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+console.log(days[date.getDay()]);
+
+//---------------------------------------------------
+
+const dateSet = new Date();
+dateSet.setFullYear(2020, 11, 3);
+console.log(dateSet);
+
+//The setDate() method can also be used to add days to a date:
+dateSet.setDate(dateSet.getDate() + 50);
+console.log(dateSet);
+
+let text = "";
+const today = new Date();
+const someday = new Date();
+someday.setFullYear(2100, 0, 14);
+
+if (someday > today) {
+  text = "Today is before January 14, 2100.";
+} else {
+  text = "Today is after January 14, 2100.";
+}
+console.log(text);
